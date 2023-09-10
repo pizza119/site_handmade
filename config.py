a33128ee15d1decd867e8cda1539fbbb445169b9
SeangBhin Yang <did030704@naver.com>
2023-09-10 21:33:44 +0900

    삭제파일 정리

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'feynman.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "y_never_know_my_secret_key_hahaha"
-
-# print(SQLALCHEMY_DATABASE_URI)
